48bdc1980d3e2ac6bd3252cd698a1b6a596772b9
Hristo Nestorov <nbglink@gmail.com>
2023-02-14 01:15:56 +0200

    Some changes to java code

--- src/main/java/com/devopskills/demoapp/DemoAppApplication.java
+++ src/main/java/com/devopskills/demoapp/DemoAppApplication.java
@@ -1,5 +1,6 @@
 package com.devopskills.demoapp;
 
+import io.dekorate.docker.annotation.DockerBuild;
 import io.dekorate.kubernetes.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,11 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @KubernetesApplication(
 		ports = @Port(name = "http", containerPort = 9090),
 		labels = @Label(key = "version", value = "v1"),
-		initContainers = @Container(image="nbglink/demo-app:jma-11", imagePullPolicy = ImagePullPolicy.Always),
+		imagePullPolicy = ImagePullPolicy.Always,
 		serviceType = ServiceType.LoadBalancer,
 		replicas = 3
 
 )
+@DockerBuild(image="nbglink/demo-app:jma-11")
 public class DemoAppApplication {
 
 	public static void main(String[] args) {
