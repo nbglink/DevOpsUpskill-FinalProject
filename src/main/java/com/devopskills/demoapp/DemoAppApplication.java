fe12a810d4b9c4f918ec854e59e2c97c8d77f8dd
Hristo <nbglink@gmail.com>
2023-02-13 23:27:25 +0000

    schange

--- src/main/java/com/devopskills/demoapp/DemoAppApplication.java
+++ src/main/java/com/devopskills/demoapp/DemoAppApplication.java
@@ -11,7 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 		labels = @Label(key = "version", value = "v1"),
 		imagePullPolicy = ImagePullPolicy.Always,
 		serviceType = ServiceType.LoadBalancer,
-		replicas = 3
+		replicas = 2
 
 )
 @DockerBuild(image="nbglink/demo-app:jma-11")
