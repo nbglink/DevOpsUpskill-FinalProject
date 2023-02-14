8b811d842ddfa9246cd25ffe6695cc476ea31136
Hristo <nbglink@gmail.com>
2023-02-14 19:04:38 +0000

    code changes

--- src/main/java/com/devopskills/demoapp/HelloController.java
+++ src/main/java/com/devopskills/demoapp/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return System.getProperty("IMAGE_NAME");
+        return "Build Number:"+System.getenv("BUILD_NUMBER");
     }
 }
