8aa9ee3b27929ec1425d07424d1fc5d893fa4bec
Hristo <nbglink@gmail.com>
2023-02-15 15:49:47 +0000

    OK

--- src/main/java/com/devopskills/demoapp/HelloController.java
+++ src/main/java/com/devopskills/demoapp/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "<h2><i><b>Hello DevOps Gurus :) !!!<b><i><h2>";
+        return "<h2>15.02.2023 <i><b>Hello DevOps Gurus :) !!!<b><i><h2>";
     }
 }
